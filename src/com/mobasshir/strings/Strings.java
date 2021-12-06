60508c5049d2303f22bff76b6ddb44bd0663512f
imobasshir <iibrahimimam24@gmail.com>
2021-12-06 23:14:04 +0530

    day 12-3

---
+++ src/com/mobasshir/strings/Strings.java
@@ -0,0 +1,8 @@
+package com.mobasshir.strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Mobasshir Imam";     // initialization and declaration
+        System.out.println(name);
+    }
+}
