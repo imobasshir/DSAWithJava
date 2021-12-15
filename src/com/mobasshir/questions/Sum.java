5fad1f55c1238227307381a7aee63a464ba4ccad
imobasshir <iibrahimimam24@gmail.com>
2021-12-15 20:34:51 +0530

    day 12-11

---
+++ src/com/mobasshir/questions/Sum.java
@@ -0,0 +1,12 @@
+package com.mobasshir.questions;
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(add(5, 6, 7));
+    }
+
+    static int add(int a, int b, int c) {
+        int sum = a + b + c;
+        return sum;
+    }
+}
