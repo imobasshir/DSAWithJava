5a82a79b8ceee43bbcbce264c7fb8976a1620e91
Mobasshir Imam <iibrahimimam24@gmail.com>
2021-12-18 19:51:21 +0530

    day 12-14

--- src/com/mobasshir/recursion/Fibonacci.java
+++ src/com/mobasshir/recursion/Fibonacci.java
@@ -2,22 +2,30 @@ package com.mobasshir.recursion;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int ans = fib(3);
-        System.out.println(ans);
+        // int ans = fib(3);
+        // System.out.println(ans);
+        // for (double i = 1; i < 11; i++) {
+        //     System.out.println(fibo(i));
+        // }
+        System.out.println(fibo(19));
+    }
+
+    static double fibo(double n) {
+        return (long) (Math.pow(((1 + Math.sqrt(5)) / 2), n)) / Math.sqrt(5);
     }
 
     static int fib(int n) {
         // base condition
         // if (n == 0) {
-        //     return 0;
+        // return 0;
         // }else if (n == 1) {
-        //     return 1;
+        // return 1;
         // }
 
         if (n < 2) {
             return n;
         }
 
-        return fib(n-1) + fib(n-2);
+        return fib(n - 1) + fib(n - 2);
     }
 }
