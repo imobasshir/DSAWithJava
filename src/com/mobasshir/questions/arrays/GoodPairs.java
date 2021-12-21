5075468c053f8b6255f3c81c9b439ef68bdfcb98
Mobasshir Imam <iibrahimimam24@gmail.com>
2021-12-22 00:14:40 +0530

    day 12-20

---
+++ src/com/mobasshir/questions/arrays/GoodPairs.java
@@ -0,0 +1,22 @@
+package com.mobasshir.questions.arrays;
+
+public class GoodPairs {
+    // https://leetcode.com/problems/number-of-good-pairs/
+    // Number of Good Pairs
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 0 };
+        System.out.println(numIdenticalPairs(arr));
+    }
+
+    static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] == nums[j] ) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ src/com/mobasshir/questions/arrays/SmallerThan.java
@@ -0,0 +1,33 @@
+package com.mobasshir.questions.arrays;
+
+import java.util.Arrays;
+
+public class SmallerThan {
+    // https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+    // How Many Numbers Are Smaller Than the Current Number
+    public static void main(String[] args) {
+        int[] nums = { 8, 1, 2, 2, 3 };
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+    }
+
+    static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] minValArr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = nums.length - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    count++;
+                }
+            }
+            // int j = nums.length - 1;
+            // while (j >= 0) {
+            // if (nums[i] > nums[j]) {
+            // counter++;
+            // }
+            // j--;
+            // }
+            minValArr[i] = count;
+        }
+        return minValArr;
+    }
+}
