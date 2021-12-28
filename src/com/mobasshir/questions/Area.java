3e232961d35a29daa9208f0b17fde21510aa0005
Mobasshir Imam <iibrahimimam24@gmail.com>
2021-12-29 00:40:06 +0530

    day 12-24

--- src/com/mobasshir/questions/Area.java
+++ src/com/mobasshir/questions/Area.java
@@ -3,12 +3,18 @@ package com.mobasshir.questions;
 public class Area {
     public static void main(String[] args) {
         System.out.println(areaCircle(5));
+        System.out.println(areaCircleWithoutMathFunction(5));
         System.out.println(areaTriangle(5, 4));
         System.out.println(areaEqTriangle(5));
     }
 
     static double areaCircle(int r) {
         double area = Math.PI * r * r;
+        return area;
+    }
+
+    static double areaCircleWithoutMathFunction(int r) {
+        double area = 3.14159265 * r * r;
         return area;
     }
 
