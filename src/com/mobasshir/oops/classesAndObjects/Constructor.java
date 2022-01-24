b2a1eb74ecd3e4c6107ee055a3ab00f987453d26
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-25 01:45:05 +0530

    day 14-5

--- src/com/mobasshir/oops/classesAndObjects/Constructor.java
+++ src/com/mobasshir/oops/classesAndObjects/Constructor.java
@@ -2,11 +2,13 @@ package com.mobasshir.oops.classesAndObjects;
 
 public class Constructor {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.greeting();
-        // System.out.println(student1.rollno);
-        // System.out.println(student1.name);
-        // System.out.println(student1.marks);
+        Student student1 = new Student(4, "Mobasshir Imam", 88f);
+        // student1.greeting();
+        System.out.println(student1.rollno);
+        System.out.println(student1.name);
+        System.out.println(student1.marks);
+        Student studentRandom = new Student();
+        System.out.println(studentRandom.marks);
     }
 }
 
@@ -22,6 +24,12 @@ class Student {
     Student() {
         this.rollno = 1;
         this.name = "Mobasshir";
-        this.marks = 79.9f;
+        this.marks = 79f;
+    }
+
+    Student(int rollno, String name, float marks) {
+        this.rollno = rollno;
+        this.name = name;
+        this.marks = marks;
     }
 }
---
+++ src/com/mobasshir/oops/classesAndObjects/WrapperExample.java
@@ -0,0 +1,10 @@
+package com.mobasshir.oops.classesAndObjects;
+
+public class WrapperExample {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        Integer num = 45;
+        System.out.println(num.toString());
+    }
+}
