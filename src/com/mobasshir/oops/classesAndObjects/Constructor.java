db43dd7e3169d091e5fa817364d1dcf1598e6e00
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-25 01:01:40 +0530

    day 14-4

--- src/com/mobasshir/oops/classesAndObjects/Constructor.java
+++ src/com/mobasshir/oops/classesAndObjects/Constructor.java
@@ -3,8 +3,9 @@ package com.mobasshir.oops.classesAndObjects;
 public class Constructor {
     public static void main(String[] args) {
         Student student1 = new Student();
-        System.out.println(student1.rollno);
-        System.out.println(student1.name);
+        student1.greeting();
+        // System.out.println(student1.rollno);
+        // System.out.println(student1.name);
         // System.out.println(student1.marks);
     }
 }
@@ -13,6 +14,10 @@ class Student {
     int rollno;
     String name;
     float marks;
+
+    void greeting() {
+        System.out.println("Hello! My name is " + name);
+    }
 
     Student() {
         this.rollno = 1;
