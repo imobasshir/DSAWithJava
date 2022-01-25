f9cf4ac74013f6a1e29cb6b794ccd6e43d0fc71e
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-25 22:43:06 +0530

    packages in java

---
+++ src/com/mobasshir/oops/a/Greet.java
@@ -0,0 +1,11 @@
+package com.mobasshir.oops.a;
+
+public class Greet {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void greet() {
+        System.out.println("Hello User");
+    }
+}
---
+++ src/com/mobasshir/oops/b/Hello.java
@@ -0,0 +1,10 @@
+package com.mobasshir.oops.b;
+
+import static com.mobasshir.oops.a.Greet.greet;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        greet();
+    }
+}
