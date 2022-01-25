3692c7d78fd557642f0e2b291f0a1e6ded665ebc
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-26 01:16:34 +0530

    day 14-8

--- src/com/mobasshir/oops/polymorphism/MethodOverloading.java
+++ src/com/mobasshir/oops/polymorphism/MethodOverloading.java
@@ -9,8 +9,14 @@ public class MethodOverloading {
         return a + b + c;
     }
 
+    int sum(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(3, 4));
         System.out.println(sum(3, 4, 5));
+        MethodOverloading sum = new MethodOverloading();
+        System.out.println(sum.sum(3, 4, 5, 6));
     }
 }
