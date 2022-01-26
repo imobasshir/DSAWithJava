46ef106bda2959b7ac80f95417fdb80d1311c38d
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-26 17:09:42 +0530

    getter mtd and private access modifier

---
+++ src/com/mobasshir/oops/accessControl/A.java
@@ -0,0 +1,21 @@
+package com.mobasshir.oops.accessControl;
+
+public class A {
+    private int num;
+    String name;
+    int[] arr;
+
+    public A(int num, String name) {
+        this.num = num;
+        this.name = name;
+        this.arr = new int[34];
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
---
+++ src/com/mobasshir/oops/accessControl/Main.java
@@ -0,0 +1,8 @@
+package com.mobasshir.oops.accessControl;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(10, "Name");
+        System.out.println(obj.getNum());
+    }
+}
