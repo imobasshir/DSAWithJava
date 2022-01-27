c804923319fcc6b1ceff8b4f8d1f4cb0f1dc0802
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-27 18:39:42 +0530

    custom array list creation and operations

---
+++ src/com/mobasshir/oops/generics/CustomArrayList.java
@@ -0,0 +1,67 @@
+package com.mobasshir.oops.generics;
+
+import java.util.Arrays;
+
+public class CustomArrayList {
+
+    private int[] data;
+    private static int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public CustomArrayList() {
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public void add(int num) {
+        if (isFull()) {
+            resize();
+        }
+        data[size++] = num;
+    }
+
+    public int remove(int num) {
+        int removed = data[--size];
+        return removed;
+    }
+
+    public int get(int index) {
+        return data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void set(int index, int value) {
+        data[index] = value;
+    }
+
+    private void resize() {
+        int[] temp = new int[data.length * 2];
+
+        // copy the current item in new array
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    public static void main(String[] args) {
+        // ArrayList list = new ArrayList<>();
+        CustomArrayList list = new CustomArrayList();
+        list.add(3);
+        list.add(5);
+        list.add(9);
+
+        System.out.println(list);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList [data=" + Arrays.toString(data) + ", size=" + size + "]";
+    }
+}
