48f56cf6186df38435d0529d37a3fe7a14195e90
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-27 22:41:46 +0530

    throw

--- src/com/mobasshir/oops/exceptionHandling/Main.java
+++ src/com/mobasshir/oops/exceptionHandling/Main.java
@@ -5,11 +5,18 @@ public class Main {
         int a = 5;
         int b = 0;
         try {
-            int c = a / b;
+            divide(a, b);
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
         } finally {
             System.out.println("This will always run");
         }
+    }
+
+    private static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("please do not divide by 0");
+        }
+        return a / b;
     }
 }
