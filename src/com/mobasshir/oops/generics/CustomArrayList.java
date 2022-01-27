50cdb3e89af04744b51ec1d7cee57cd490a63bd6
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-27 18:42:05 +0530

    array list custom more oprations

--- src/com/mobasshir/oops/generics/CustomArrayList.java
+++ src/com/mobasshir/oops/generics/CustomArrayList.java
@@ -53,9 +53,13 @@ public class CustomArrayList {
     public static void main(String[] args) {
         // ArrayList list = new ArrayList<>();
         CustomArrayList list = new CustomArrayList();
-        list.add(3);
-        list.add(5);
-        list.add(9);
+        // list.add(3);
+        // list.add(5);
+        // list.add(9);
+
+        for (int i = 0; i < 14; i++) {
+            list.add(2*i);
+        }
 
         System.out.println(list);
     }
