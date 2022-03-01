7e9e8f91e7b0857d4815d02c66a9b164b5e4aaea
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-03-02 00:12:09 +0530

    array list

--- src/com/mobasshir/array/ArrayListExample.java
+++ src/com/mobasshir/array/ArrayListExample.java
@@ -7,24 +7,24 @@ public class ArrayListExample {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(5);
         Scanner in = new Scanner(System.in);
-        // list.add(678);
+        list.add(678);
         // list.add(607);
         // list.add(6147);
         // list.add(4567);
         // list.add(6987);
         // list.add(967);
         // System.out.println(list.contains(607));
-        // System.out.println(list);
+        System.out.println(list);
         // list.set(3, 55);
         // System.out.println(list);
         // list.remove(0);
         // System.out.println(list);
-        for(int i=0;i<5;i++){
-            list.add(in.nextInt());
-        }
-        System.out.println(list);
-        for(int i=0;i<5;i++){
-            System.out.println(list.get(i));
-        }
+        // for(int i=0;i<5;i++){
+        //     list.add(in.nextInt());
+        // }
+        // System.out.println(list);
+        // for(int i=0;i<5;i++){
+        //     System.out.println(list.get(i));
+        // }
     }
 }
