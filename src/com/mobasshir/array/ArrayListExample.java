207f505ff47dfc110d66165ca9254fb0f5046cc4
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-03-04 00:05:47 +0530

    arraylist2

--- src/com/mobasshir/array/ArrayListExample.java
+++ src/com/mobasshir/array/ArrayListExample.java
@@ -1,19 +1,19 @@
 package com.mobasshir.array;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(5);
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
         list.add(678);
         list.add(607);
-        // list.add(6147);
-        // list.add(4567);
-        // list.add(6987);
-        // list.add(967);
-        System.out.println(list.contains(607));
+        list.add(6147);
+        list.add(4567);
+        list.add(6987);
+        list.add(967);
+        // System.out.println(list.contains(607));
         System.out.println(list);
         // list.set(3, 55);
         // System.out.println(list);
