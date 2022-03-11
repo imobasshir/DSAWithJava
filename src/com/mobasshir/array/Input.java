9a45ef9b8c817000065e29507b6720878ef6c71c
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-03-12 00:02:58 +0530

    input

--- src/com/mobasshir/array/Input.java
+++ src/com/mobasshir/array/Input.java
@@ -7,7 +7,7 @@ public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         // primitives
-        // int[] arr = new int[5];
+        int[] arr = new int[5];
         // arr[0] = 23;
         // arr[1] = 45;
         // arr[2] = 203;
@@ -17,25 +17,26 @@ public class Input {
 
         // Taking input
         // for primitives
-        // for(int i=0;i<arr.length;i++){
-        //     arr[i] = in.nextInt();
-        // }
-        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
         // for non primitives
         String[] str = new String[4];
-        for(int i=0;i<str.length;i++){
+        for (int i = 0; i < str.length; i++) {
             str[i] = in.next();
         }
         System.out.println(Arrays.toString(str));
-        
+
         // Printing output
         // for(int i=0;i<arr.length;i++){
-        //     System.out.print(arr[i] + " ");
+        // System.out.print(arr[i] + " ");
         // }
 
-        // // enhanced for loop or for each loop4 
+        // // enhanced for loop or for each loop4
         // for (int element : arr) {
-        //     System.out.print(element + " ");
+        // System.out.print(element + " ");
         // }
 
         // toString method
