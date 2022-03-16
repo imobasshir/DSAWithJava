6f3a14e66527054856929f70f1429fdf2e103642
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-03-16 10:46:43 +0530

    insertion sort done

--- src/com/mobasshir/sorting/Sorting.java
+++ src/com/mobasshir/sorting/Sorting.java
@@ -5,9 +5,22 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] arr = { 7, 8, 3, 1, 2 };
-        // bubbleSort(arr);     // time complexity O(n^2)
-        selectionSort(arr);     // time complexity O(n^2)
+        // bubbleSort(arr); // time complexity O(n^2)
+        // selectionSort(arr); // time complexity O(n^2)
+        insertionSort(arr); // time complexity O(n^2)
         System.out.println(Arrays.toString(arr));
+    }
+
+    private static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
     }
 
     private static void selectionSort(int[] arr) {
