5ead2ad8f87b17b3d895ceed610f166067402a38
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-03-18 17:35:39 +0530

    character array

---
+++ src/com/mobasshir/strings/CharArrays.java
@@ -0,0 +1,10 @@
+package com.mobasshir.strings;
+
+public class CharArrays {
+
+    public static void main(String args[]) {
+        char[] str = { 'M', 'o', 'b', 'a', 's', 's', 'h', 'i', 'r', ' ', 'I', 'm', 'a', 'm' };
+        System.out.println(str);
+    }
+
+}
