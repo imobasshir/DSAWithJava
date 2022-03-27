9ce40ddc49041022ac7c6a0ec0430cc3eb1ef9d0
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-03-27 19:07:00 +0530

    max product subarray

---
+++ src/com/mobasshir/array/MaxProduct.java
@@ -0,0 +1,22 @@
+package com.mobasshir.array;
+
+public class MaxProduct {
+    public static void main(String[] args) {
+        int[] arr = { 2, 5, -9, 8, 6, 0, 4 };
+        System.out.println(maxProduct(arr));
+    }
+
+    static int maxProduct(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            int currentProd = 1;
+            for (int j = i; j < arr.length; j++) {
+                currentProd *= arr[j];
+                if (currentProd > max) {
+                    max = currentProd;
+                }
+            }
+        }
+        return max;
+    }
+}
