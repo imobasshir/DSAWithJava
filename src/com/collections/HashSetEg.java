3ca2b866066c8b678cafb74735d8ebb8b80aedc6
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-08 00:00:22 +0530

    hash set

---
+++ src/com/collections/HashSetEg.java
@@ -0,0 +1,23 @@
+package com.collections;
+
+import java.util.HashSet;
+
+public class HashSetEg {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 1; i <= 5; i++) {
+            set.add(i);
+        }
+        System.out.println(set);
+        set.add(4);
+        set.add(6);
+        System.out.println(set);
+        set.remove(5);
+        System.out.println(set);
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+        System.out.println(set.contains(3));
+        set.clear();
+        System.out.println(set);
+    }
+}
