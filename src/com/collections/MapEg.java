96131e4f36f3362a9ffd2da109e06b652a15b579
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-08 00:19:47 +0530

    map

---
+++ src/com/collections/MapEg.java
@@ -0,0 +1,32 @@
+package com.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapEg {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Mobasshir", 1);
+        map.put("Btech", 2);
+        System.out.println(map);
+        map.putIfAbsent("Mobasshir", 2);
+        System.out.println(map);
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            System.out.println(entry);
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+        for(String key: map.keySet()) {
+            System.out.println(key);
+        }
+        for (Integer i : map.values()) {
+            System.out.println(i);
+        }
+        System.out.println(map.containsKey("Btech"));
+        System.out.println(map.containsValue(22));
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        map.clear();
+        System.out.println(map);
+    }
+}
