f920c0443ead13c487854af044e4e1cb84fbbd20
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-23 20:17:40 +0530

    cycle detection

--- src/com/mobasshir/linkedlist/implementaion/singly/CycleDetection.java
+++ src/com/mobasshir/linkedlist/implementaion/singly/CycleDetection.java
@@ -44,4 +44,34 @@ public class CycleDetection {
         }
         return 0;
     }
+
+    // cycleStart
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        int length = 0;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthCycle(head);
+                break;
+            }
+        }
+        if (length == 0) {
+            return null;
+        }
+        ListNode f = head;
+        ListNode s = head;
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
 }
