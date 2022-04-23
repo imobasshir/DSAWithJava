c4c19115e0290b9cda3376c0f118070a5ee44c86
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-23 21:23:21 +0530

    some problems on linked list

--- src/com/mobasshir/linkedlist/implementaion/singly/CycleDetection.java
+++ src/com/mobasshir/linkedlist/implementaion/singly/CycleDetection.java
@@ -49,29 +49,19 @@ public class CycleDetection {
     public ListNode detectCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        int length = 0;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
             if (fast == slow) {
-                length = lengthCycle(head);
-                break;
+                fast = head;
+                // now put fast again to start and slow on where it is
+                // now travel both the pointer once  and they will meet at starting pts
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
             }
         }
-        if (length == 0) {
-            return null;
-        }
-        ListNode f = head;
-        ListNode s = head;
-        while (length > 0) {
-            s = s.next;
-            length--;
-        }
-
-        while (f != s) {
-            f = f.next;
-            s = s.next;
-        }
-        return s;
+        return null;
     }
 }
---
+++ src/com/mobasshir/linkedlist/implementaion/singly/HappyNo.java
@@ -0,0 +1,29 @@
+package com.mobasshir.linkedlist.implementaion.singly;
+
+public class HappyNo {
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = findSq(slow);
+            fast = findSq(findSq(fast));
+        } while(slow != fast);
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int findSq(int n) {
+        int ans = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            ans += rem * rem;
+            n /= 10;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+    }
+}
