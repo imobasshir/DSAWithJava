9ee6930d8889e0af94a4a8c3b11836a84b3ce39b
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-23 20:01:25 +0530

    cycle detection in ll

---
+++ src/com/mobasshir/linkedlist/implementaion/singly/CycleDetection.java
@@ -0,0 +1,47 @@
+package com.mobasshir.linkedlist.implementaion.singly;
+
+public class CycleDetection {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // find length of cycle
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != fast);
+
+                return length;
+            }
+        }
+        return 0;
+    }
+}
