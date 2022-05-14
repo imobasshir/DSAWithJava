4555d740c69e25ffef012659167f261ba4a5b9da
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-05-14 22:03:21 +0530

    max heap delete

--- src/com/mobasshir/heap/MaxHeap.java
+++ src/com/mobasshir/heap/MaxHeap.java
@@ -5,26 +5,43 @@ import java.util.Arrays;
 public class MaxHeap {
     public static void main(String[] args) {
         int[] arr = new int[20];
-        for (int i = 0; i < 6; i++) {
-            arr[0] = 50;
-            arr[1] = 30;
-            arr[2] = 40;
-            arr[3] = 10;
-            arr[4] = 5;
-            arr[5] = 20;
-        }
+        arr[1] = 50;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 10;
+        arr[5] = 5;
+        arr[6] = 20;
         int value = 60;
         int n = 6;
-        add(arr, n, value);
+        delete(arr, n);
+        // add(arr, n, value);
         System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void delete(int[] arr, int n) {
+        arr[1] = arr[n];
+        n = n - 1;
+        int i = 1;
+        while (i < n) {
+            int left = arr[2 * i];
+            int right = arr[2 * i] + 1;
+            int large = left > right ? 2 * i : 2 * i + 1;
+            if (arr[i] < arr[large]) {
+                swap(arr, large, i);
+                i = large;
+            } else {
+                return;
+            }
+        }
     }
 
     private static void add(int[] arr, int n, int value) {
         n = n + 1;
-        arr[n - 1] = value;
-        int i = n - 1;
+        arr[n] = value;
+        int i = n;
         while (i > 0) {
-            int parent = (n - 1) / 2;
+            int parent = (n) / 2;
             if (arr[i] > arr[parent]) {
                 swap(arr, parent, i);
                 i = parent;
