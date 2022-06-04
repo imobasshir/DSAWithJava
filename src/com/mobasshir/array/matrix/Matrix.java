a8bd5a2e9f1d8838c7953ea9b11dd4b68b15b4fb
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-06-04 13:49:31 +0530

    matrix

--- src/com/mobasshir/array/matrix/Matrix.java
+++ src/com/mobasshir/array/matrix/Matrix.java
@@ -2,6 +2,11 @@ package com.mobasshir.array.matrix;
 
 public class Matrix {
     public static void main(String[] args) {
-        // int[][] mat = new int[4][];
+        int[][] mat = new int[4][3];
+        for(int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                
+            }
+        }
     }
 }
