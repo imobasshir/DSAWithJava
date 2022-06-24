d80e8002a9d56def5d7e4bf9b5c7c1ef40670e03
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-06-24 21:14:51 +0530

    sliding window

---
+++ src/com/mobasshir/array/slidingWindow/FirstNegativeNo.java
@@ -0,0 +1,60 @@
+package com.mobasshir.array.slidingWindow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FirstNegativeNo {
+    public static void main(String[] args) {
+        int[] arr = { 12, -1, -7, 8, -18, 30, 16, 28 };
+        int k = 3;
+        System.out.println(firstNegativeBrute(arr, k));
+        System.out.println(firstNegative(arr, k));
+    }
+
+    static ArrayList<Integer> firstNegative(int[] arr, int k) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        // boolean flag = false;
+        int i = 0;
+        int j = 0;
+        while (j < arr.length) {
+            if (arr[j] < 0) {
+                list.add(arr[j]);
+            }
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+                if (list.size() == 0) {
+                    ans.add(0);
+                } else {
+                    ans.add(list.get(0));
+                    if (arr[i] == list.get(0)) {
+                        list.remove(0);
+                    }
+                }
+                i++;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+
+    static ArrayList<Integer> firstNegativeBrute(int[] arr, int k) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean flag = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < i + k && j < arr.length; j++) {
+                if (arr[j] < 0) {
+                    flag = true;
+                    ans.add(arr[j]);
+                    break;
+                }
+            }
+            if (!flag) {
+                ans.add(0);
+            }
+        }
+        return ans;
+    }
+}
--- src/com/mobasshir/array/slidingWindow/MaxOfSubarray.java
+++ src/com/mobasshir/array/slidingWindow/MaxOfSubarray.java
@@ -6,17 +6,53 @@ public class MaxOfSubarray {
         int[] arr = { 2, 3, 5, 2, 9, 7, 1 };
         int n = 3;
 
-        // Brute Force
+        System.out.println(bruteForce(arr, n));
+        System.out.println(bruteForceMin(arr, n));
+        System.out.println(usingSlidingWindow(arr, n));
+    }
+
+    static int bruteForce(int[] arr, int k) {
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
             int sum = 0;
-            for (int j = i; j < i + n && (j < arr.length); j++) {
+            for (int j = i; j < i + k && (j < arr.length); j++) {
                 sum += arr[j];
             }
             if (sum > max) {
                 max = sum;
             }
         }
-        System.out.println(max);
+        return max;
+    }
+
+    static int bruteForceMin(int[] arr, int k) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k && (j < arr.length); j++) {
+                sum += arr[j];
+            }
+            min = Math.min(sum, min);
+        }
+        return min;
+    }
+
+    static int usingSlidingWindow(int[] arr, int k) {
+        int i = 0;
+        int j = 0;
+        int max = 0;
+        int sum = 0;
+        while (j < arr.length) {
+            sum += arr[j];
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+                max = Math.max(sum, max);
+                sum -= arr[i];
+                i++;
+                j++;
+            }
+        }
+        return max;
     }
 }
