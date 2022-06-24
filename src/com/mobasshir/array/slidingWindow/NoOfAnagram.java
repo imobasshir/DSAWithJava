ea6682f9d10b5f2182baaec3f06fad0e652d74b0
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-06-25 01:42:10 +0530

    anagram

--- src/com/mobasshir/array/slidingWindow/NoOfAnagram.java
+++ src/com/mobasshir/array/slidingWindow/NoOfAnagram.java
@@ -9,6 +9,7 @@ public class NoOfAnagram {
         String s = "aabaabaabaa";
         String ptr = "aaba";
         System.out.println(anagram(s, ptr));
+        // System.out.println(anagramCt(s, ptr));
     }
 
     static int anagram(String s, String ptr) {
@@ -52,5 +53,65 @@ public class NoOfAnagram {
             }
         }
         return occurences;
+    }
+
+    static int anagramCt(String s, String ptr) {
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> ptrmap = new HashMap<>();
+
+        // Store all the pattern value in map
+        for (int l = 0; l < ptr.length(); l++) {
+            ptrmap.put(ptr.charAt(l), ptrmap.getOrDefault(ptr.charAt(l), 0) + 1);
+        }
+
+        // Store all the s value of value size in map
+        for (int l = 0; l < ptr.length(); l++) {
+            smap.put(s.charAt(l), smap.getOrDefault(s.charAt(l), 0) + 1);
+        }
+
+        int i = 0;
+        int j = ptr.length();
+        int count = 0;
+        while (j < s.length()) {
+            if (compare(ptrmap, smap) == true) {
+                count++;
+            }
+
+            smap.put(s.charAt(j), smap.getOrDefault(s.charAt(j), 0) + 1);
+
+            if (smap.get(s.charAt(i)) == 1) { 
+                smap.remove(s.charAt(i));
+            } else {
+                smap.put(s.charAt(i), smap.get(s.charAt(i)) - 1);
+            }
+        }
+        if (compare(ptrmap, smap) == true) {
+            count++;
+        }
+
+        return count;
+    }
+
+    static boolean compare(Map<Character, Integer> mapA, Map<Character, Integer> mapB) {
+        // for (char sch : smap.keySet()) {
+        //     if (ptrmap.getOrDefault(sch, 0) != smap.get(sch)) {
+        //         return false;
+        //     }
+        // }
+
+        // for (char k : mapB.keySet())
+        // {
+        //     if (!mapA.get(k).equals(mapB.get(k))) {
+        //         return false;
+        //     }
+        // } 
+        // for (char y : mapA.keySet())
+        // {
+        //     if (!mapB.containsKey(y)) {
+        //         return false;
+        //     }
+        // } 
+        boolean b = mapA.entrySet().stream().filter(value -> mapB.entrySet().stream().anyMatch(value1 -> (value1.getKey() == value.getKey() && value1.getValue() == value.getValue()))).findAny().isPresent();
+        return b;
     }
 }
