c0c1bb16b3631fede83615f39fa44ebb4acb8ff6
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-06-27 04:46:35 +0530

    queue

--- src/com/mobasshir/queue/UsingLinkedList.java
+++ src/com/mobasshir/queue/UsingLinkedList.java
@@ -1,7 +1,65 @@
 package com.mobasshir.queue;
 
 public class UsingLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmpty() {
+            return head == null & tail == null;
+        }
+
+        public static void add(int data) {
+            Node newNode = new Node(data);
+            if (tail == null) {
+                tail = head = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        public static int remove() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int front = head.data;
+            if (head == tail) {
+                tail = null;
+            }
+            head = head.next;
+
+            return front;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
     public static void main(String[] args) {
-    
+        Queue queue = new Queue();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.peek());
+            queue.remove();
+        }
     }
 }
