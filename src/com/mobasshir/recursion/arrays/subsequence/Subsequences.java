f3dbea4422bcaee55b0a70ef8772c444745e721a
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-08-03 02:11:58 +0530

    subsequence

--- src/com/mobasshir/recursion/arrays/subsequence/Subsequences.java
+++ src/com/mobasshir/recursion/arrays/subsequence/Subsequences.java
@@ -4,27 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subsequences {
+    static List<List<Integer>> ans = new ArrayList<>();
+
     public static void main(String[] args) {
         int[] arr = { 3, 1, 2 };
         List<Integer> list = new ArrayList<>();
         subsequences(arr, 0, 3, list);
+        System.out.println(ans);
     }
 
     static void subsequences(int[] arr, int i, int n, List<Integer> list) {
         if (i == n) {
             if (list.size() > 0)
-                System.out.println(list);
+                ans.add(new ArrayList<>(list));
+            // System.out.println(list);
             if (list.size() == 0)
-                System.out.println("[]");
+                ans.add(new ArrayList<>());
+            // System.out.println("[]");
             return;
         } else {
             // picked
-            subsequences(arr, i + 1, n, list);
             list.add(arr[i]);
+            subsequences(arr, i + 1, n, list);
 
             // not picked
-            subsequences(arr, i + 1, n, list);
             list.remove(list.size() - 1);
+            subsequences(arr, i + 1, n, list);
         }
     }
 }
