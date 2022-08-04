02107170bc31059f02961e678f8d8eea3a7aa6c0
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-08-05 00:10:19 +0530

    n queens

--- src/com/mobasshir/recursion/arrays/NQueens.java
+++ src/com/mobasshir/recursion/arrays/NQueens.java
@@ -6,22 +6,16 @@ import java.util.List;
 
 public class NQueens {
     public static void main(String[] args) {
-        System.out.println(solveNQueens(4));
+        int n = 4;
+        helper(new ArrayList<String>(), 0, new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1], n);
     }
 
-    private static List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<List<String>>();
-        helper(result, new ArrayList<String>(), 0, new boolean[n], new boolean[2 * n], new boolean[2 * n], n);
-        return result;
-    }
-
-    private static void helper(List<List<String>> result, List<String> board, int row, boolean[] cols, boolean[] upperDiognal,
-            boolean[] lowerDiognal, int n) {
+    private static void helper(List<String> board, int row, boolean[] cols, boolean[] upperDiognal, boolean[] lowerDiognal, int n) {
         if (row == n) {
-            result.add(new ArrayList<String>(board));
+            System.out.print(board + " ");
         }
         for (int col = 0; col < n; col++) {
-            int fillUpperDiognal = col - row + n;
+            int fillUpperDiognal = col - row + n - 1;
             int fillLowerDiognal = col + row;
             if (!cols[col] && !upperDiognal[fillUpperDiognal] && !lowerDiognal[fillLowerDiognal]) {
                 char[] r = new char[n];
@@ -31,7 +25,7 @@ public class NQueens {
                 cols[col] = true;
                 upperDiognal[fillUpperDiognal] = true;
                 lowerDiognal[fillLowerDiognal] = true;
-                helper(result, board, row + 1, cols, upperDiognal, lowerDiognal, n);
+                helper(board, row + 1, cols, upperDiognal, lowerDiognal, n);
                 board.remove(board.size() - 1);
                 cols[col] = false;
                 upperDiognal[fillUpperDiognal] = false;
