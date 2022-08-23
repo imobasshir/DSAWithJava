3e0f589ba7b691156dca68c97fd0533d8295c7a5
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-08-24 00:58:49 +0530

    patterns

--- src/com/mobasshir/pattern/Patterns.java
+++ src/com/mobasshir/pattern/Patterns.java
@@ -13,7 +13,14 @@ public class Patterns {
         // patterns9(5);
         // patterns10(5);
         // patterns11(5);
-        patterns12(5);
+        // patterns12(5);
+        // patterns13(5);
+        // patterns14(5);
+        // patterns15(5);
+        // patterns16(5);
+        // patterns17(5);
+        // patterns18(5);
+        patterns19(5);
     }
 
     static void patterns1(int n) {
@@ -158,6 +165,96 @@ public class Patterns {
             for (int j = 1; j <= i; j++) {
                 System.out.print(flag + " ");
                 flag = flag * (i - j) / j;
+            }
+            System.out.println();
+        }
+    }
+
+    static void patterns13(int n) {
+        int alphabet = 65;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void patterns14(int n) {
+        int alphabet = 97;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void patterns15(int n) {
+        int alphabet = 65;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            alphabet++;
+            System.out.println();
+        }
+    }
+
+    static void patterns16(int n) {
+        int alphabet = 65;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (alphabet + i) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void patterns17(int n) {
+        for (int i = 0; i < n; i++) {
+            int alphabet = 65;
+            for (int j = 0; j < n - i; j++) {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            alphabet++;
+            System.out.println();
+        }
+    }
+
+    static void patterns18(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void patterns19(int n) {
+        for (int i = 1; i < n * 2; i++) {
+            for (int j = 1; j < n * 2; j++) {
+                // int k = 1, l = 0;
+                // if (i == k || j == k || i == n * 2 - k || j == n * 2 - k) {
+                // System.out.print(n - l + " ");
+                // }
+                // k++;
+                // l++;
+                if (i == 1 || i == n * 2 - 1 || j == 1 || j == n * 2 - 1) {
+                    System.out.print(n + " ");
+                } else if (i == 2 || j == 2 || i == n * 2 - 2 || j == n * 2 - 2) {
+                    System.out.print(n - 1 + " ");
+                } else if (i == 3 || j == 3 || i == n * 2 - 3 || j == n * 2 - 3) {
+                    System.out.print(n - 2 + " ");
+                } else if (i == 4 || j == 4 || i == n * 2 - 4 || j == n * 2 - 4) {
+                    System.out.print(n - 3 + " ");
+                } else if (i == 5 || j == 5 || i == n * 2 - 5 || j == n * 2 - 5) {
+                    System.out.print(n - 4 + " ");
+                }
             }
             System.out.println();
         }
