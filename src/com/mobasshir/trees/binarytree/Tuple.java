3ce7e80ab81e934dc8615d62d2ec4e328591773c
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-08-26 00:44:04 +0530

    binary tree

---
+++ src/com/mobasshir/trees/binarytree/Tuple.java
@@ -0,0 +1,12 @@
+package com.mobasshir.trees.binarytree;
+
+public class Tuple {
+    public Node node;
+    public int row;
+    public int col;
+    public Tuple(Node _node, int _row, int _col) {
+        node = _node;
+        row = _row;
+        col = _col;
+    }
+}
