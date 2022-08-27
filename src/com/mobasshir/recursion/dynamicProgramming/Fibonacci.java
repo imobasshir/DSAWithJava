aa1ce688709361ebdd4146bef984e6d577d86f4f
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-08-28 00:08:58 +0530

    fibo dp

--- src/com/mobasshir/recursion/dynamicProgramming/Fibonacci.java
+++ src/com/mobasshir/recursion/dynamicProgramming/Fibonacci.java
@@ -1,20 +1,28 @@
 package com.mobasshir.recursion.dynamicProgramming;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 4;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        System.out.println(fibo(n, dp));
+        int n = 6;
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+        // System.out.println(fibo(n, dp));
+        int prev2 = 0;
+        int prev = 1;
+        for(int i = 2; i <= n; i++) {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        System.out.println(prev);
     }
 
-    private static int fibo(int n, int[] dp) {
-        if (n <= 1) return n;
+    // private static int fibo(int n, int[] dp) {
+    //     if (n <= 1) return n;
 
-        if (dp[n] != -1) return dp[n];
+    //     if (dp[n] != -1) return dp[n];
 
-        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
-    }
+    //     return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+    // }
 }
