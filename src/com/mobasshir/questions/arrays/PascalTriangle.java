25752ee74c446679fa00ef75c454ee0636e1c85d
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-12-02 00:01:57 +0530

    done

--- src/com/mobasshir/questions/arrays/PascalTriangle.java
+++ src/com/mobasshir/questions/arrays/PascalTriangle.java
@@ -7,6 +7,8 @@ public class PascalTriangle {
     public static void main(String[] args) {
         System.out.println(generate(5));
         System.out.println(getRow(3));
+        int[][] ans = helper(5);
+        ans(ans);
     }
 
     public static List<List<Integer>> generate(int numRows) {
@@ -38,5 +40,26 @@ public class PascalTriangle {
         }
         list.add(1);
         return list;
+    }
+
+    public static int[][] helper(int n) {
+        int[][] ans = new int[n][];
+        for (int i = 0; i < n; i++) {
+            ans[i] = new int[i + 1];
+            ans[i][0] = ans[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+        }
+        return ans;
+    }
+
+    public static void ans(int[][] ans) {
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[i].length; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
